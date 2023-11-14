59fc58e81d3081a4dea20bbeccfa642bd1b39fc6
seminss <2007ksm@naver.com>
2023-11-14 23:45:30 +0900

    refactor: input 에러 핸들링 로직을 InputView로 이동
    supplier을 사용해 함수로 valid input을 반환하도록 설정

--- src/main/java/christmas/view/input/InputView.java
+++ src/main/java/christmas/view/input/InputView.java
@@ -8,20 +8,27 @@ import christmas.view.input.validator.VisitDateValidator;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class InputView {
 
-    private InputView() {
-
+    public static <T> T getValidInput(Supplier<T> inputSupplier) {
+        while (true) {
+            try {
+                return inputSupplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    public static Integer readVisitDate(){
+    public static Integer readVisitDate() {
         String userInput = read();
         VisitDateValidator.validate(userInput);
         return VisitDateParser.parseInteger(userInput);
     }
 
-    public static List<SimpleEntry<String, Integer>> readOrder(){ //TODO: 넘길 DTO 만들기
+    public static List<SimpleEntry<String, Integer>> readOrder() { //TODO: 넘길 DTO 만들기
         String userInput = read();
         OrderMenuValidator.validate(userInput);
         return OrderParser.parseEachMenu(userInput);
@@ -34,6 +41,4 @@ public class InputView {
     private static String read() {
         return Console.readLine();
     }
-
-
 }
